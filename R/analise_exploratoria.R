bf553628f5ce0dc7954d636b0517c19a1218eefa
mikael04 <mikael.coletto.eng@gmail.com>
2022-09-06 10:33:00 -0300

    Adicionando analise para dois generos mais vistos juntos; Iniciando analise de desenvolvedores;

--- R/analise_exploratoria.R
+++ R/analise_exploratoria.R
@@ -10,11 +10,11 @@ library(dplyr)
 library(data.table)
 library(dtplyr)
 
-# 1. Lendo os dados ----
+# 1. Dados de generos ----
 dados <- read.table("data-raw/steam-data/applicationGenres.csv", col.names = paste("V",1:25), fill = T, sep = ",")
 dados <- dados[,which(!is.na(dados[1,]))]
 
-## 1.1 Organizando os nomes das colunas
+## 1.1.1 Organizando os nomes das colunas
 colnames(dados) <- c("ID", "Categoria1", "Categoria2", "Categoria3", "Categoria4",
                      "Categoria5", "Categoria6", "Categoria7", "Categoria8")
 
@@ -63,7 +63,7 @@ func_count_genres <- function(df){
   df_counts
 }
 
-## Criando tabela de contagens para cada gênero
+## 1.1. Tabela de contagens para cada gênero ----
 df_counts <- func_count_genres(df)
 
 ## Gráfico de barras com os 10 gêneros que mais aparecem
@@ -72,23 +72,84 @@ df_counts_top10 <- df_counts |>
   dplyr::slice_head(n = 10) |> 
   tidyr::pivot_wider(names_from = genres, values_from = count)
 
-library(ggplot2)
-
 df_counts_top10 <- df_counts |> 
   dplyr::arrange(desc(count)) |> 
   dplyr::slice_head(n = 10)
 
+
+### 1.1.1. Gráfico top10 gêneros ----
+library(ggplot2)
+
 fig_top10 <- ggplot(df_counts_top10, aes(x = count, y = genres)) +
-  geom_col()
-fig
+  geom_col() +
+  theme_minimal()
+fig_top10
 
 df_counts_last10 <- df_counts |> 
   dplyr::arrange(count) |> 
   dplyr::slice_head(n = 10)
 
-## Gráfico com conjunto de dois gêneros mais vistos em conjunto
+## 1.2. Dois gêneros mais vistos em conjunto ----
 
 df_sample <- df |> 
   dplyr::sample_n(size = 30)
 
+sum(df_sample[, "Action"] & df_sample[,4] == T)
+sum(df_double)
 df_sample[,3] == T
+df_sample[,4] == T
+df_sample[,2]
+
+top10_genres <- df_counts_top10 |> 
+  dplyr::select(genres) |> 
+  dplyr::pull()
+
+
+## Criando dfs base para contagem de comparativo
+df_comp<- data.frame(matrix(ncol = 2, nrow = 0))
+colnames(df_comp) <- c("genres", "count")
+k <- 1
+for(i in 1:length(top10_genres)){
+  print(paste0("Comecando a buscar duplas no genero ", top10_genres[i]))
+  for(j in 2:(ncol(df)-1)){
+    print(paste0("Comparando com o genero ", all_genres[j]))
+    if(all_genres[j] != top10_genres[i]){
+      print(paste0("i = ", i))
+      print(paste0("j = ", j))
+      count <- sum(df[, i] & df[,j] == T)
+      this_comp <- c(paste0(top10_genres[i], " & ", all_genres[j]), count)
+      df_comp[k, ] = this_comp
+      k=k+1
+    }
+  }
+}
+
+df_comp[,2] = as.numeric(df_comp[,2])
+
+
+df_duplas_top10 <- df_comp |> 
+  dplyr::arrange(desc(count)) |> 
+  dplyr::slice_head(n = 10)
+
+### 1.2.1. Gráfico top10 dois gêneros agrupados ----
+
+fig_top10_dup <- ggplot(df_duplas_top10, aes(x = count, y = reorder(genres, count))) +
+  geom_col() +
+  theme_minimal()
+fig_top10_dup
+
+rm(list=ls())
+
+# 2. Dados de desenvolvedores ----
+
+df <- read.csv2("data-raw/steam-data/applicationDevelopers.csv", col.names = paste("V",1:15), fill = T, sep = ",")
+df <- df[,which(!is.na(df[1,]))]
+df <- df[,2:8]
+all_developers <- c("")
+for(i in 1:ncol(df)){
+  all_developers <- unique(c(all_developers, unique(df[,i])))
+}
+
+df_aux <- as.matrix(df[,1])
+df_aux <- append(df_aux, df[,2], after = length(df_aux))
+all_developers
