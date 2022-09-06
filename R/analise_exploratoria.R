4e4f74ecf890a1704b7c3ec9b58fe5d12a85cdcc
mikael04 <mikael.coletto.eng@gmail.com>
2022-09-06 17:27:59 -0300

    Adicionando gráfico de empresas (mas preciso tratar antes os nomes);

--- R/analise_exploratoria.R
+++ R/analise_exploratoria.R
@@ -144,12 +144,48 @@ rm(list=ls())
 
 df <- read.csv2("data-raw/steam-data/applicationDevelopers.csv", col.names = paste("V",1:15), fill = T, sep = ",")
 df <- df[,which(!is.na(df[1,]))]
+## Removendo primeira coluna com "identificador"
 df <- df[,2:8]
+
+## Criando vetor com valores únicos de desenvolvedores
 all_developers <- c("")
 for(i in 1:ncol(df)){
   all_developers <- unique(c(all_developers, unique(df[,i])))
 }
 
+## Criando df/vetor com todas as colunas
 df_aux <- as.matrix(df[,1])
-df_aux <- append(df_aux, df[,2], after = length(df_aux))
-all_developers
+for(i in 2:ncol(df)){
+  df_aux <- append(df_aux, df[,i], after = length(df_aux))
+}
+df_aux <- as.data.frame(df_aux) |> 
+  dplyr::filter(df_aux != "") |> 
+  dplyr::rename(devs = df_aux)
+
+## Limpando dados dúbios e pontuações
+
+## Removendo variação Linux/Mac mesma empresa
+df_all_words <- stringr::str_remove(df_aux$devs, "Linux")
+df_all_words <- stringr::str_remove(df_all_words, "Mac")
+df_all_words <- as.data.frame(df_all_words) |> 
+  dplyr::rename(devs = df_all_words)
+
+df_all_words_check <- df_all_words |> 
+  dplyr::filter(stringr::str_detect(devs, "2K"))
+
+## Limpando pontuações
+df_all_words <- gsub("[[:punct:]]", "", df_all_words)
+library(dplyr)
+df_all_devs_count <- df_all_words |> 
+  dplyr::group_by(df_all_words) |> 
+  dplyr::summarize(count = n()) |> 
+  dplyr::ungroup() |> 
+  dplyr::arrange(desc(count)) |> 
+  dplyr::slice_head(n = 10) |> 
+  dplyr::rename(devs = df_all_words)
+
+
+fig_top10_dev <- ggplot(df_all_devs_count, aes(x = count, y = reorder(devs, count))) +
+  geom_col() +
+  theme_minimal()
+fig_top10_dev
